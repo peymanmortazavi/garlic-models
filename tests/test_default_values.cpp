9833685605b121a4c4766f1c181ee95da6699477
Peyman <pey.mortazavi@gmail.com>
2019-11-29 03:06:24 -0500

    move away from storing file info in root CMakeLists.txt

---
+++ tests/test_default_values.cpp
@@ -0,0 +1,13 @@
+/******************************************************************************
+* File:             test_default_values.cpp
+*
+* Author:           Peyman Mortazavi
+* Created:          11/25/19
+* Description:      Tests out default value implementations (garlic::object, garlic::string, etc.)
+*****************************************************************************/
+
+#include <gtest/gtest.h>
+
+
+TEST(DefaultValue, StringValue) {
+}
