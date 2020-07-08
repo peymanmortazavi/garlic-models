1a5b0f086760c8acc4b2ee6b0a35354b46a8c738
Peyman <pey.mortazavi@gmail.com>
2020-07-07 22:11:51 -0400

    Clean test protocol.

--- tests/test_protocol.h
+++ tests/test_protocol.h
@@ -18,32 +18,9 @@
 #include <vector>
 
 
-template<typename T>
-void assert_no_list(T& value) {
-  //ASSERT_FALSE(value.is_list());
-  //ASSERT_THROW(value.get_list().begin(), garlic::TypeError);
-  //ASSERT_THROW(value.get_list().end(), garlic::TypeError);
-  //ASSERT_THROW(value.append(T::none), garlic::TypeError);
-  //ASSERT_THROW(value.remove(0), garlic::TypeError);
-  //ASSERT_THROW(value[0], garlic::TypeError);
-}
-
-
-template<typename T>
-void assert_no_object(T& value) {
-  //ASSERT_FALSE(value.is_object());
-  //ASSERT_THROW(value.set("Peyman", T::none), garlic::TypeError);
-  //ASSERT_THROW(value.get("Peyman"), garlic::TypeError);
-  //ASSERT_THROW(value.get_object().begin(), garlic::TypeError);
-  //ASSERT_THROW(value.get_object().end(), garlic::TypeError);
-}
-
-
 template<garlic::ReadableLayer LayerType>
 void test_readonly_string_value(const LayerType& value, const std::string& text) {
   ASSERT_TRUE(value.is_string());
-  //assert_no_object(value);
-  //assert_no_list(value);
 
   std::string std_string = value.get_string();
   const char* c_str = value.get_cstr();
@@ -84,24 +61,4 @@ void test_readonly_list_range(const LayerType& value) {
     it++;
   }
   ASSERT_EQ(it, value.end_list());
-}
-
-/**
- * value must be an object with ability creating child elements.
- * testing that a string value would return cstr, std string and std string view.
- */
-template<typename T>
-void test_writable_object_string_capabilities(T& value) {
-  std::string texts[] = {"Peyman", "Some other name just to make sure it can deal with it."};
-
-  std::for_each(begin(texts), end(texts), [&](auto text) {
-      value.set("name", text.c_str());
-      check_value("name", text);
-
-      std::string key = "name_str_string";
-      value.set(key, text);
-      auto str = value.get(key);
-      test_readonly_string_value(str, text);
-    }
-  );
 }
