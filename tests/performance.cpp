561680948fa330b690cdd608e4c866fa5be6b31c
Peyman <pey.mortazavi@gmail.com>
2020-12-14 06:53:09 -1000

    add json module loading tests.

--- tests/performance.cpp
+++ tests/performance.cpp
@@ -2,16 +2,26 @@
 #include <benchmark/benchmark.h>
 #include <garlic/garlic.h>
 #include "garlic/providers/libyaml.h"
+#include "garlic/providers/rapidjson.h"
 #include "test_utility.h"
 
 
-static void BM_StringCreation(benchmark::State& state) {
+static void BM_LibYamlModuleLoad(benchmark::State& state) {
   for (auto _ : state) {
     garlic::Module<garlic::providers::libyaml::YamlView> module;
     load_libyaml_module(module, "data/performance/module.yaml");
   }
 }
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+BENCHMARK(BM_LibYamlModuleLoad);
+
+static void BM_LoadRapidJsonModule(benchmark::State& state) {
+  garlic::Module<garlic::providers::rapidjson::JsonView> module;
+  load_libyaml_module(module, "data/performance/module.yaml");
+  auto doc = get_rapidjson_document("data/performance/test1.json");
+  for (auto _ : state) {
+    module.get_model("Module")->validate(doc.get_view());
+  }
+}
+BENCHMARK(BM_LoadRapidJsonModule);
 
 BENCHMARK_MAIN();
