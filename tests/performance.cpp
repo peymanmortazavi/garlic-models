33e714cb3f5bf9a3b60ff30104b6662af9a05978
Peyman <pey.mortazavi@gmail.com>
2021-01-11 17:51:41 -1000

    Add more to the performance tests.

--- tests/performance.cpp
+++ tests/performance.cpp
@@ -1,8 +1,10 @@
+#include <cstring>
 #include <iostream>
 #include <benchmark/benchmark.h>
 #include <garlic/garlic.h>
 #include "garlic/providers/libyaml.h"
 #include "garlic/providers/rapidjson.h"
+#include "rapidjson/document.h"
 #include "test_utility.h"
 
 
@@ -24,5 +26,58 @@ static void BM_LoadRapidJsonModule(benchmark::State& state) {
   }
 }
 BENCHMARK(BM_LoadRapidJsonModule);
+
+static auto CreateLargeRapidJsonDocument() {
+  rapidjson::Document d;
+  d.SetArray();
+  for (size_t i = 0; i < 1000; i++) {
+    auto object = rapidjson::Value();
+    object.SetObject();
+    object.AddMember(
+        "text",
+        "This is a long text just for the purpose of testing.",
+        d.GetAllocator());
+    object.AddMember("value", 18231, d.GetAllocator());
+    d.PushBack(std::move(object), d.GetAllocator());
+  }
+  return d;
+}
+
+static void BM_LoadRapidJsonDocument_Native(benchmark::State& state) {
+  auto doc = CreateLargeRapidJsonDocument();
+  long long character_count = 0;
+  long long total = 0;
+  for (auto _ : state) {
+    for (const auto& object_it : doc.GetArray()) {
+      if (auto y = object_it.FindMember("text"); y != object_it.MemberEnd()) {
+        character_count += strlen(y->value.GetString());
+      }
+      if (auto y = object_it.FindMember("value"); y != object_it.MemberEnd()) {
+        total += y->value.GetInt();
+      }
+    }
+  }
+  printf("%lld - %lld\n", character_count, total);
+}
+BENCHMARK(BM_LoadRapidJsonDocument_Native);
+
+static void BM_LoadRapidJsonDocument_Garlic(benchmark::State& state) {
+  garlic::providers::rapidjson::JsonDocument doc {CreateLargeRapidJsonDocument()};
+  long long character_count = 0;
+  long long total = 0;
+  for (auto _ : state) {
+    auto view = doc.get_view();
+    for (const auto& object_it : view.get_list()) {
+      if (auto y = object_it.find_member("text"); y != object_it.end_member()) {
+        character_count += strlen((*y).value.get_cstr());
+      }
+      if (auto y = object_it.find_member("value"); y != object_it.end_member()) {
+        total += (*y).value.get_int();
+      }
+    }
+  }
+  printf("%lld - %lld\n", character_count, total);
+}
+BENCHMARK(BM_LoadRapidJsonDocument_Garlic);
 
 BENCHMARK_MAIN();
