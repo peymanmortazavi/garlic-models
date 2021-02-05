d2f8490191a71ca36ad4215f63258876ce67843f
Peyman <pey.mortazavi@gmail.com>
2021-02-04 23:26:00 -1000

    Add all three ways of decoding.

--- include/garlic/encoding.h
+++ include/garlic/encoding.h
@@ -6,10 +6,19 @@
 
 namespace garlic {
 
-  template<typename, typename>
-  struct decoder : std::false_type {};
+  template<typename Type, typename Layer>
+  struct coder {};
 
   /* TODO:  <04-02-21, Peyman> use concept for c++ and onward. */
+
+  template<typename, typename, class = void>
+  struct has_explicit_decoder : std::false_type {};
+
+  template<typename Type, typename Layer>
+  struct has_explicit_decoder<
+    Type,
+    Layer,
+    std::void_t<decltype(coder<Type, Layer>::decode(std::declval<Layer>()))>> : std::true_type {};
 
   template<typename, typename, class = void>
   struct is_layer_constructible : std::false_type{};
@@ -29,15 +38,33 @@ namespace garlic {
     Layer,
     std::void_t<decltype(Type::decode(std::declval<Layer>()))>> : std::true_type {};
 
+  namespace internal {
+    template<typename Type, typename Layer>
+    constexpr bool use_explicit_decoder = has_explicit_decoder<Type, Layer>::value;
+
+    template<typename Type, typename Layer>
+    constexpr bool use_decode_layer_method =
+      has_decode_layer_method<Type, Layer>::value
+      && !has_explicit_decoder<Type, Layer>::value;
+
+    template<typename Type, typename Layer>
+    constexpr bool use_decode_constructor = 
+      is_layer_constructible<Type, Layer>::value
+      && !has_decode_layer_method<Type, Layer>::value
+      && !has_explicit_decoder<Type, Layer>::value;
+  }
+
   template<typename Type, ViewLayer Layer>
-  static inline std::enable_if_t<has_decode_layer_method<Type, Layer>::value, Type>
+  static inline std::enable_if_t<internal::use_decode_layer_method<Type, Layer>, Type>
   decode(Layer layer) { return Type::decode(layer); }
 
   template<typename Type, ViewLayer Layer>
-  static inline std::enable_if_t<
-    is_layer_constructible<Type, Layer>::value && !has_decode_layer_method<Type, Layer>::value,
-    Type>
+  static inline std::enable_if_t<internal::use_decode_constructor<Type, Layer>, Type>
   decode(Layer layer) { return Type(layer); }
+
+  template<typename Type, ViewLayer Layer>
+  static inline std::enable_if_t<internal::use_explicit_decoder<Type, Layer>, Type>
+  decode(Layer layer) { return coder<Type, Layer>::decode(layer); }
 
   namespace internal {
 
--- tests/test_encoding.cpp
+++ tests/test_encoding.cpp
@@ -3,6 +3,7 @@
 #include <garlic/providers/rapidjson.h>
 #include <garlic/encoding.h>
 #include <garlic/clove.h>
+#include <memory>
 
 #include "test_utility.h"
 
@@ -14,6 +15,18 @@ TEST(Encoding, DefaultLayerCopy) {
   CloveDocument target;
   copy_layer(doc.get_view(), target.get_reference());
   ASSERT_TRUE(cmp_layers(doc.get_view(), target.get_view()));
+}
+
+inline static CloveRef get_test_clove() {
+  static std::unique_ptr<CloveDocument> doc;
+  if (!doc) {
+    doc = std::make_unique<CloveDocument>();
+    auto ref = doc->get_reference();
+    ref.set_object();
+    ref.add_member("prop1", "Property 1");
+    ref.add_member("prop2", 24);
+  }
+  return doc->get_reference();
 }
 
 struct Data1 {
@@ -41,24 +54,35 @@ public:
   int prop2;
 };
 
+struct Data3 {
+  std::string prop1;
+  int prop2;
+};
+
+template<typename Layer>
+struct coder<Data3, Layer> {
+  static inline Data3 decode(Layer layer) {
+    return {
+      (*layer.find_member("prop1")).value.get_string(),
+      (*layer.find_member("prop2")).value.get_int(),
+    };
+  }
+};
+
 TEST(Encoding, CustomStaticClassDecoder) {
-  CloveDocument doc;
-  auto target = doc.get_reference();
-  target.set_object();
-  target.add_member("prop1", "Property 1");
-  target.add_member("prop2", 24);
-  auto data = decode<Data1>(target);
+  auto data = decode<Data1>(get_test_clove());
   ASSERT_STREQ(data.prop1.c_str(), "Property 1");
   ASSERT_EQ(data.prop2, 24);
 }
 
 TEST(Encoding, CustomConstructorClassDecoder) {
-  CloveDocument doc;
-  auto target = doc.get_reference();
-  target.set_object();
-  target.add_member("prop1", "Property 1");
-  target.add_member("prop2", 24);
-  auto data = decode<Data2, CloveRef>(target);
+  auto data = decode<Data2>(get_test_clove());
+  ASSERT_STREQ(data.prop1.c_str(), "Property 1");
+  ASSERT_EQ(data.prop2, 24);
+}
+
+TEST(Encoding, CustomExplicitDecoder) {
+  auto data = decode<Data3>(get_test_clove());
   ASSERT_STREQ(data.prop1.c_str(), "Property 1");
   ASSERT_EQ(data.prop2, 24);
 }
