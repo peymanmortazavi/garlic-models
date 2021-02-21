57afe8b5b07ea9d11b2d1f811dd93e8dcb7fafcf
Peyman <pey.mortazavi@gmail.com>
2021-02-21 11:31:46 -1000

    Added tests for garlic::sequence

--- tests/test_containers.cpp
+++ tests/test_containers.cpp
@@ -1,8 +1,11 @@
+#include <algorithm>
 #include <cstring>
 #include <gtest/gtest.h>
 #include <garlic/containers.h>
+#include <iterator>
 #include <string>
 
+using namespace std;
 using namespace garlic;
 
 TEST(GarlicText, ConstCharConstructor) {
@@ -55,4 +58,58 @@ TEST(GarlicText, Copy) {
 
 TEST(GarlicText, StaticNoText) {
   ASSERT_EQ(text::no_text().data(), text::no_text().data());
+}
+
+
+TEST(GarlicSequence, Basic) {
+  sequence<double> numbers;  // default ctor
+  numbers.push_back(12);  // rvalue
+  double x = 13;
+  numbers.push_back(x);  // lvalue
+  const double& z = ++x;
+  numbers.push_back(z);
+  double target[3] = {12, 13, 14};
+  ASSERT_EQ(numbers.size(), 3);
+  ASSERT_TRUE(equal(begin(numbers), end(numbers), begin(target)));
+}
+
+TEST(GarlicSequence, Growth) {
+  sequence<double> numbers(2);
+  static const auto total = 1000000;
+  double target[total];
+  for (auto i = 0; i < 1000000; ++i) {
+    target[i] = i;
+    numbers.push_back(i);
+  }
+  ASSERT_EQ(numbers.size(), total);
+  ASSERT_TRUE(equal(begin(numbers), end(numbers), begin(target)));
+}
+
+TEST(GarlicSequence, BackInserter) {
+  struct Data {
+    long double d;
+    string s;
+    Data* another = nullptr;
+  };
+  sequence<Data> items;
+  vector<Data> target {};
+  for (auto i = 0; i< 100; ++i) target.push_back(Data{static_cast<long double>(i), std::to_string(i)});
+  copy(begin(items), end(items), back_inserter(items));
+}
+
+TEST(GarlicSequence, MoveOperators) {
+  sequence<int> numbers(12);
+  numbers.push_back(1);
+  numbers.push_back(2);
+  auto another_one = std::move(numbers);  // move ctor.
+  ASSERT_EQ(numbers.capacity(), 0);
+  ASSERT_EQ(another_one.capacity(), 12);
+  ASSERT_EQ(another_one.size(), 2);
+
+  sequence<int> new_one(2);
+  new_one.push_back(1);
+  another_one = std::move(new_one);
+  ASSERT_EQ(new_one.capacity(), 0);
+  ASSERT_EQ(another_one.capacity(), 2);
+  ASSERT_EQ(another_one.size(), 1);
 }
