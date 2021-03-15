b3e4d48a667b0516948ac83ef9cb335cdfde167e
Peyman <pey.mortazavi@gmail.com>
2021-03-15 12:53:18 -1000

    Added documentation for modules.

--- include/garlic/module.h
+++ include/garlic/module.h
@@ -1,6 +1,11 @@
 #ifndef GARLIC_MODULE_H
 #define GARLIC_MODULE_H
 
+/*!
+ * \file module.h
+ * \brief A repository of models, fields and constraints that can be serialized/deserialized.
+ */
+
 #include <unordered_map>
 
 #include "lib/expected.h"
@@ -12,6 +17,27 @@
 
 namespace garlic {
 
+  /*! A Module is a repository of models, fields and constraints.
+   *
+   *  This allows to package lots of relevant fields and models in one object.
+   *  This can be parsed and dumped which makes it easy to save in a file or send it over the wire.
+   *
+   *  In a module, no two records can exist with the same entity and they cannot be overriden.
+   *  So if a Field has already been added by the name "Field", this cannot be exchanged for any
+   *  other Field. This is mostly because other Model or Constraint instances might already depend on it.
+   *
+   *  \code{.cpp}
+   *  garlic::Module module;
+   *  module.add_field(garlic::make_field("ID", { garlic::make_constraint<garlic::regex_tag>("\\d{1,3}") }));
+   *  module.add_field("UID", module.get_field("ID"));  // an alias for field ID.
+   *  auto user_model = garlic::make_model("User");
+   *  user_model->add_field("id", module.get_field("UID"));
+   *  module.add_model(user_model);
+   *  \endcode
+   *
+   *  \note Try not to add models or fields that depend on resources outside of this module.
+   *        That would make it impossible to deserialize should that become necessary.
+   */
   class Module {
   public:
     template <typename Key, typename Value> using table = std::unordered_map<Key, Value>;
@@ -27,6 +53,9 @@ namespace garlic {
 
     constexpr static auto kDefaultFieldTableSize = 16;
 
+    /*! Creates an empty module with the following fields already available:
+     *  **string**, **bool**, **list**, **object**, **integer**, **double**
+     */
     Module() : fields_(kDefaultFieldTableSize) {
       static table<text, field_pointer> static_map = {
         {"string", this->make_field<type_tag>("StringField", TypeFlag::String)},
@@ -39,50 +68,88 @@ namespace garlic {
       fields_ = static_map;
     }
 
-    tl::expected<void, std::error_code> add_model(model_pointer model) noexcept {
+    /*! Tries to add a new model to the module.
+     *
+     * \param model A shared pointer to the Model.
+     * \return nothing if successful, std::error_code if a model by the same name already exists.
+     */
+    tl::expected<void, std::error_code>
+    add_model(model_pointer model) noexcept {
       if (auto it = models_.find(model->name()); it != models_.end())
         return tl::make_unexpected(GarlicError::Redefinition);
       models_.emplace(model->name().view(), std::move(model));
       return tl::expected<void, std::error_code>();
     }
 
-    tl::expected<void, std::error_code> add_field(text&& alias, field_pointer field) noexcept {
+    /*! Tries to add a new field to the module.
+     *
+     * \param alias A custom name or an alias to use for the field.
+     * \param field A shared pointer to the Field.
+     * \return nothing if successful, std::error_code if a field record by the same alias already exists.
+     */
+    tl::expected<void, std::error_code>
+    add_field(text&& alias, field_pointer field) noexcept {
       if (auto it = fields_.find(alias); it != fields_.end())
         return tl::make_unexpected(GarlicError::Redefinition);
       fields_.emplace(std::move(alias), std::move(field));
       return tl::expected<void, std::error_code>();
     }
 
-    inline tl::expected<void, std::error_code> add_field(field_pointer field) noexcept {
+    /*! Tries to add a new field to the module, using its name for the field record.
+     *
+     * \param field A shared pointer to the Field.
+     * \return nothing if successful, std::error_code if a field record by the same alias already exists.
+     */
+    inline tl::expected<void, std::error_code>
+    add_field(field_pointer field) noexcept {
       return add_field(field->name().view(), field);
     }
 
+    /*! Get a model by its name.
+     *
+     * \return a shared pointer to the model if found, otherwise nullptr.
+     */
     model_pointer get_model(const text& name) const noexcept {
       if (auto it = models_.find(name); it != models_.end()) return it->second;
       return nullptr;
     }
 
+    /*! Get a model by its name.
+     *  Calls the callback function with a shared pointer to the Model if found.
+     */
     template<typename Callable>
     void get_model(const text& name, Callable&& cb) const noexcept {
       if (auto it = models_.find(name); it != models_.end()) cb(it->second);
     }
 
+    //! \return a read-only (const) iterator that points to the first model in the module.
     inline const_model_iterator begin_models() const { return models_.begin(); }
+
+    //! \return a read-only (const) iterator that points to one past the last model in the module.
     inline const_model_iterator end_models() const { return models_.end(); }
+
+    //! \return a read-only (const) iterator that points to found element or **end_models()** if not found.
     inline const_model_iterator find_model(const text& name) const { return models_.find(name); }
 
+    //! \return a shared pointer to the found field or nullptr if not found.
     field_pointer get_field(const text& name) const noexcept {
       if (auto it = fields_.find(name); it != fields_.end()) return it->second;
       return nullptr;
     }
 
+    //! Calls the callable function with a shared pointer to the field, if found.
     template<typename Callable>
     void get_field(const text& name, Callable&& cb) const noexcept {
       if (auto it = fields_.find(name); it != fields_.end()) cb(it->second);
     }
 
+    //! \return a read-only (const) iterator that points to the first field in the module.
     inline const_field_iterator begin_fields() const { return fields_.begin(); }
+
+    //! \return a read-only (const) iterator that points to one past the last field in the module.
     inline const_field_iterator end_fields() const { return fields_.end(); }
+
+    //! \return a read-only (const) iterator that points to found element or **end_fields()** if not found.
     inline const_field_iterator find_field(const text& name) const { return fields_.find(name); }
 
   private:
